07ee3c0ebe91ffbfa4a2ddda85a14dfce8c2a0b7
andreasantillana <andreasantillana@gmail.com>
2014-01-15 17:56:37 -0600

    Adding probesize and analyzeduration for late streams

--- lib/ffmpeg/movie.rb
+++ lib/ffmpeg/movie.rb
@@ -13,7 +13,7 @@ module FFMPEG
       @path = path
 
       # ffmpeg will output to stderr
-      command = "#{FFMPEG.ffmpeg_binary} -i #{Shellwords.escape(path)}"
+      command = "#{FFMPEG.ffmpeg_binary} -probesize 2G -analyzeduration 2G -i #{Shellwords.escape(path)}"
       output = Open3.popen3(command) { |stdin, stdout, stderr| stderr.read }
 
       fix_encoding(output)
